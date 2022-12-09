6ff4773f4e680ee016563aea4ed624d2bd577f2d
Yeison Fernandez <yeison.fernandez.14@sansano.usm.cl>
2022-12-09 12:27:59 -0300

    update hash router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import {
-  BrowserRouter as Router, Route, Routes
+  HashRouter as Router, Route, Routes
 } from "react-router-dom";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Menu from './components/Menu/Menu';
 import Navbar from './components/Navbar/Navbar';
